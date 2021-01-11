0476f78c60a9c9fbb0118b30b98b9e400e95bb5d
dengxiaolin <dengxiaolin@meituan.com>
2021-01-11 14:14:01 +0800

    java agent

--- trace-agent/src/main/java/com/trace/agent/JvmAgent.java
+++ trace-agent/src/main/java/com/trace/agent/JvmAgent.java
@@ -3,6 +3,7 @@ package com.trace.agent;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 
+
 /**
  * 利用JDK1.6的动态代理
  *
@@ -10,13 +11,36 @@ import java.lang.instrument.Instrumentation;
  * @since 2021/01/10
  */
 public class JvmAgent {
+    private static Instrumentation instr;
+
+    /**
+     * 命令行使用
+     */
+    public static void premain(String agentArgs, Instrumentation instrumentation) {
+        System.out.println("agentmain agent start");
+        instr = instrumentation;
+        install();
+        System.out.println("agentmain agent end");
+    }
+
     /**
      * 类加载调用
      */
     public static void agentmain(String agentArgs, Instrumentation instrumentation) {
-        System.out.println("premain agent start");
-        ClassFileTransformer transformer = new TraceClassFileTransformer();
-        instrumentation.addTransformer(transformer, true);
-        System.out.println("premain agent end");
+        System.out.println("agentmain agent start");
+        instr = instrumentation;
+        install();
+        System.out.println("agentmain agent end");
     }
+
+    private static void install() {
+        ClassFileTransformer transformer = new TraceClassFileTransformer();
+        instrumentation().addTransformer(transformer, true);
+    }
+
+    private static Instrumentation instrumentation() {
+        return instr;
+    }
+
+
 }
