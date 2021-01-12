0a6d8923f9692837f0c6bbec360340aec2997c28
dengxiaolin <dengxiaolin@meituan.com>
2021-01-12 10:05:03 +0800

    java agent

--- trace-agent/src/main/java/com/trace/agent/transform/ThreadPoolTransformer.java
+++ trace-agent/src/main/java/com/trace/agent/transform/ThreadPoolTransformer.java
@@ -83,7 +83,7 @@ public class ThreadPoolTransformer implements TraceTransformer {
                 String paraTypeName = paraType.getName();
 
                 if (CLASS_NAME_MAP.containsKey(paraTypeName)) {
-                    System.out.println("interfaceName:" + paraTypeName);
+                    System.out.println("paraTypeName:" + paraTypeName);
                     int paramIndex = i + 1;
                     String replaceCode = String.format("$%d = %s.getInstance($%d);", paramIndex, CLASS_NAME_MAP.get(paraTypeName), paramIndex);
                     sb.append(replaceCode);
