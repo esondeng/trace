53d5a92a345861adc67739e1faeefc57f54d20c2
dengxiaolin <dengxiaolin@meituan.com>
2021-01-14 20:22:42 +0800

    mybatis trace

--- trace-core/src/main/java/com/trace/core/aop/TraceAspect.java
+++ trace-core/src/main/java/com/trace/core/aop/TraceAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import com.trace.core.Span;
 import com.trace.core.TraceContext;
 import com.trace.core.enums.ServiceType;
 import com.trace.core.manager.TraceManager;
@@ -27,14 +28,16 @@ public class TraceAspect {
     @Around("tracePointCut()")
     public Object traceAdvice(ProceedingJoinPoint point) throws Throwable {
         // 内部调用时，验证是否在Trace context下
-        if (TraceContext.get() == null) {
+        Span parentSpan = TraceContext.get();
+        if (parentSpan == null) {
             return point.proceed();
         }
         else {
             Method method = ((MethodSignature) point.getSignature()).getMethod();
             String name = method.getDeclaringClass().getSimpleName() + "." + method.getName();
 
-            return TraceManager.tracingWithReturn(ServiceType.INNER_CALL, name, point::proceed);
+            ServiceType serviceType = parentSpan.isAsyncParent() ? ServiceType.ASYNC : ServiceType.INNER_CALL;
+            return TraceManager.tracingWithReturn(serviceType, name, point::proceed);
         }
     }
 }
