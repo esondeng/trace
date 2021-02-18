67a5b04d44b1db4be6d50ed81479f19e2e498324
dengxiaolin <dengxiaolin@meituan.com>
2021-02-18 17:19:30 +0800

    日志收集

---
+++ trace-common/src/main/java/com/trace/common/domain/IndexLog.java
@@ -0,0 +1,18 @@
+package com.trace.common.domain;
+
+/**
+ * @author dengxiaolin
+ * @since 2021/02/18
+ */
+public class IndexLog {
+    private String appKey;
+    private String ip;
+    private String traceId;
+
+    private String loggerName;
+    private String thread;
+    private long logTime;
+    private String logLevel;
+
+    private String message;
+}
