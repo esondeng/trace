44a6a91b54d7bb81af8b2584e2e2ca6b4f7ed5cd
dengxiaolin <dengxiaolin@meituan.com>
2021-02-18 10:29:56 +0800

    trace 详情页

--- trace-monitor/src/main/resources/static/js/trace-detail.js
+++ trace-monitor/src/main/resources/static/js/trace-detail.js
@@ -14,6 +14,54 @@ $(function () {
 
     hideSameJvm();
 
+    function hideSpan(id){
+        const span = $("[id='"+id+"']");
+        span.hide();
 
+        if($(span).get(0).hasAttribute("data-child-ids")){
+            const childIds = $(span).get(0).getAttribute("data-child-ids");
+            childIds.substring(1,childIds.length-1).split(",").forEach(childId => {
+                hideSpan(childId);
+            });
+        }
+    }
 
+    function showSpan(id){
+        const span = $("[id='"+id+"']");
+        span.show();
+
+        const expander = $(span).find(".expander");
+        if(expander){
+            $(expander).text("-");
+        }
+
+        if($(span).get(0).hasAttribute("data-child-ids")){
+            const childIds = $(span).get(0).getAttribute("data-child-ids");
+            childIds.substring(1,childIds.length-1).split(",").forEach(childId => {
+                showSpan(childId);
+            });
+        }
+    }
+
+    $(".expander").on("click", function (e) {
+        e.preventDefault();
+        const parentSpan = $(e.target).parent().parent().parent().get(0);
+        const childIds = parentSpan.getAttribute("data-child-ids");
+        const showText = $(e.target).text();
+
+        if (childIds) {
+            if (showText === "-") {
+                $(e.target).text("+");
+                childIds.substring(1,childIds.length-1).split(",").forEach(childId => {
+                    hideSpan(childId.trim());
+                });
+            }
+            else {
+                $(e.target).text("-");
+                childIds.substring(1,childIds.length-1).split(",").forEach(childId => {
+                    showSpan(childId.trim());
+                });
+            }
+        }
+    });
 });
