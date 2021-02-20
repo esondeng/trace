c38de4d4da9e7863a77a64d4ee055feb0d6fa774
dengxiaolin <dengxiaolin@meituan.com>
2021-02-20 16:29:06 +0800

    日志查询

--- trace-index/src/test/java/com/trace/index/es/EsClientTest.java
+++ trace-index/src/test/java/com/trace/index/es/EsClientTest.java
@@ -22,85 +22,9 @@ public class EsClientTest extends BaseTest {
     @Test
     void bulkTestSpans() {
         List<IndexSpan> indexSpanList = new ArrayList<>();
-        indexSpanList.addAll(JsonUtils.parseList("[{\n" +
-                "\t\"traceId\": \"198fc58b4ad74188ae1bcd44130e265e\",\n" +
-                "\t\"name\": \"ProductController.test\",\n" +
-                "\t\"id\": \"0\",\n" +
-                "\t\"depth\": 1,\n" +
-                "\t\"appKey\": \"dubbo-consumer-demo\",\n" +
-                "\t\"ip\": \"172.25.161.78\",\n" +
-                "\t\"start\": 1612869248074,\n" +
-                "\t\"end\": 1612869250910,\n" +
-                "\t\"cost\": 2836,\n" +
-                "\t\"serviceType\": \"http\",\n" +
-                "\t\"status\": \"success\",\n" +
-                "\t\"tagMap\": {\n" +
-                "\t\t\"httpPath\": \"/test/dubbo/test\",\n" +
-                "\t\t\"method\": \"GET\",\n" +
-                "\t\t\"httpUrl\": \"/test/dubbo/test\"\n" +
-                "\t}\n" +
-                "}, {\n" +
-                "\t\"traceId\": \"198fc58b4ad74188ae1bcd44130e265e\",\n" +
-                "\t\"name\": \"Test.test\",\n" +
-                "\t\"id\": \"0.1\",\n" +
-                "\t\"depth\": 2,\n" +
-                "\t\"appKey\": \"dubbo-consumer-demo\",\n" +
-                "\t\"ip\": \"172.25.161.78\",\n" +
-                "\t\"start\": 1612869248100,\n" +
-                "\t\"end\": 1612869248108,\n" +
-                "\t\"cost\": 8,\n" +
-                "\t\"serviceType\": \"inner-call\",\n" +
-                "\t\"status\": \"success\"\n" +
-                "}, {\n" +
-                "\t\"traceId\": \"198fc58b4ad74188ae1bcd44130e265e\",\n" +
-                "\t\"name\": \"ProductService.sayHello\",\n" +
-                "\t\"id\": \"0.2\",\n" +
-                "\t\"depth\": 2,\n" +
-                "\t\"appKey\": \"dubbo-consumer-demo\",\n" +
-                "\t\"ip\": \"172.25.161.78\",\n" +
-                "\t\"start\": 1612869248118,\n" +
-                "\t\"end\": 1612869250884,\n" +
-                "\t\"cost\": 2766,\n" +
-                "\t\"serviceType\": \"duubo-c\",\n" +
-                "\t\"status\": \"success\"\n" +
-                "}]", IndexSpan.class));
+        indexSpanList.addAll(JsonUtils.parseList("[{\"traceId\":\"fc4f3253f8374ef1a6af01b60ed8dd1b\",\"name\":\"ProductController.test\",\"id\":\"0\",\"depth\":1,\"appKey\":\"dubbo-consumer-demo\",\"ip\":\"172.25.161.78\",\"start\":1613809512895,\"end\":1613809512943,\"cost\":48,\"serviceType\":\"http\",\"errorMessage\":\"[\\\"com.dubbo.example.product.service.ProductService.sayHello : ArrayIndexOutOfBoundsException\\\"]\",\"status\":\"failed\",\"tagMap\":{\"httpPath\":\"/test/dubbo/test\",\"httpMethod\":\"GET\",\"httpUrl\":\"/test/dubbo/test\"}},{\"traceId\":\"fc4f3253f8374ef1a6af01b60ed8dd1b\",\"name\":\"Test.test\",\"id\":\"0.1\",\"depth\":2,\"appKey\":\"dubbo-consumer-demo\",\"ip\":\"172.25.161.78\",\"start\":1613809512895,\"end\":1613809512896,\"cost\":1,\"serviceType\":\"inner-call\",\"status\":\"success\"},{\"traceId\":\"fc4f3253f8374ef1a6af01b60ed8dd1b\",\"name\":\"ProductService.sayHello\",\"id\":\"0.2\",\"depth\":2,\"appKey\":\"dubbo-consumer-demo\",\"ip\":\"172.25.161.78\",\"start\":1613809512896,\"end\":1613809512941,\"cost\":45,\"serviceType\":\"duubo-c\",\"status\":\"success\"}] \n", IndexSpan.class));
 
-        indexSpanList.addAll(JsonUtils.parseList("[{\n" +
-                "\t\"traceId\": \"198fc58b4ad74188ae1bcd44130e265e\",\n" +
-                "\t\"name\": \"ProductService.sayHello\",\n" +
-                "\t\"id\": \"0.2.1\",\n" +
-                "\t\"depth\": 3,\n" +
-                "\t\"clientAppKey\": \"dubbo-consumer-demo\",\n" +
-                "\t\"clientIp\": \"172.25.161.78\",\n" +
-                "\t\"appKey\": \"dubbo-provider-demo\",\n" +
-                "\t\"ip\": \"172.25.161.78\",\n" +
-                "\t\"start\": 1612869248273,\n" +
-                "\t\"end\": 1612869250864,\n" +
-                "\t\"cost\": 2591,\n" +
-                "\t\"serviceType\": \"dubbo-p\",\n" +
-                "\t\"status\": \"success\",\n" +
-                "\t\"tagMap\": {\n" +
-                "\t\t\"request\": \"[]\"\n" +
-                "\t}\n" +
-                "}, {\n" +
-                "\t\"traceId\": \"198fc58b4ad74188ae1bcd44130e265e\",\n" +
-                "\t\"name\": \"TestMapper.getAll\",\n" +
-                "\t\"id\": \"0.2.1.1\",\n" +
-                "\t\"depth\": 4,\n" +
-                "\t\"clientAppKey\": \"dubbo-consumer-demo\",\n" +
-                "\t\"clientIp\": \"172.25.161.78\",\n" +
-                "\t\"appKey\": \"dubbo-provider-demo\",\n" +
-                "\t\"ip\": \"172.25.161.78\",\n" +
-                "\t\"start\": 1612869250804,\n" +
-                "\t\"end\": 1612869250857,\n" +
-                "\t\"cost\": 53,\n" +
-                "\t\"serviceType\": \"jdbc\",\n" +
-                "\t\"status\": \"success\",\n" +
-                "\t\"tagMap\": {\n" +
-                "\t\t\"jdbcRef\": \"jdbc:mysql://10.24.90.86:5002/waimai_agent\",\n" +
-                "\t\t\"sql\": \"select id, name, valid, ctime, utime from wm_agent_test; Total: 19\"\n" +
-                "\t}\n" +
-                "}]", IndexSpan.class));
+        indexSpanList.addAll(JsonUtils.parseList("[{\"traceId\":\"fc4f3253f8374ef1a6af01b60ed8dd1b\",\"name\":\"ProductService.sayHello\",\"id\":\"0.2.1\",\"depth\":3,\"clientAppKey\":\"dubbo-consumer-demo\",\"clientIp\":\"172.25.161.78\",\"appKey\":\"dubbo-provider-demo\",\"ip\":\"172.25.161.78\",\"start\":1613809512897,\"end\":1613809512939,\"cost\":42,\"serviceType\":\"dubbo-p\",\"errorMessage\":\"[\\\"java.lang.ArrayIndexOutOfBoundsException: 1\\\",\\\"com.dubbo.example.product.manager.ProductManager.test(ProductManager.java:25)\\\",\\\"com.dubbo.example.product.service.ProductServiceImpl.sayHello(ProductServiceImpl.java:21)\\\"]\",\"status\":\"failed\",\"tagMap\":{\"request\":\"[]\"}},{\"traceId\":\"fc4f3253f8374ef1a6af01b60ed8dd1b\",\"name\":\"TestMapper.getAll\",\"id\":\"0.2.1.1\",\"depth\":4,\"clientAppKey\":\"dubbo-consumer-demo\",\"clientIp\":\"172.25.161.78\",\"appKey\":\"dubbo-provider-demo\",\"ip\":\"172.25.161.78\",\"start\":1613809512898,\"end\":1613809512938,\"cost\":40,\"serviceType\":\"jdbc\",\"status\":\"success\",\"tagMap\":{\"jdbcRef\":\"jdbc:mysql://10.24.90.86:5002/waimai_agent\",\"sql\":\"select id, name, valid, ctime, utime from wm_agent_test; Total: 19\"}}]\n", IndexSpan.class));
 
         esClient.bulkUploadSpans(indexSpanList);
     }
